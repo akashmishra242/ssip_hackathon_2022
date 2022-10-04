0f317811b7da9fdf21fd6e946d38543f9a970e3d
Akash Mishra <akashmishra242@gmail.com>
2022-10-04 14:00:56 +0530

    Project initialized on Firebase(android ios & web)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ssip_hackathon_2022/firebase_options.dart';
 
 import 'home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
